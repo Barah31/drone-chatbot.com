43294ca75f597e7d06b9ed80a6b0911498e09e33
Barah31 <154500240+Barah31@users.noreply.github.com>
2023-12-21 15:35:10 +0800

    Add files via upload

---
+++ GROUP2/RESPONSE.js
@@ -0,0 +1,16 @@
+function getResponse (input) {
+	if (input == "rock") {
+		return "paper";
+	}else if (input == "paper") {
+		return "scissors";
+	}else if (input == "scissors") {
+		return "rock";
+	}
+	if(input == "hello") {
+		return "hello there!";
+	}else if (input == "goodbye") {
+		return "talk to you later!";
+	}else{ 
+		return"try asking something else!";
+	}
+}
